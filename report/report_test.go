d6cbbeefe419ffb2edd892156302ece4f2722107
Matheus Alcantara <matheusssilv97@gmail.com>
2020-12-17 10:10:25 -0300

    Create tests to export report data

--- report/report_test.go
+++ report/report_test.go
@@ -1,11 +1,92 @@
 package report_test
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/insidersec/insider/report"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestReportResumeConsole(t *testing.T) {
+	r := report.Report{
+		DRA: []report.DRA{{
+			Data:     "testing",
+			Type:     "email",
+			FilePath: "foo/bar",
+		}},
+		Vulnerabilities: []report.Vulnerability{
+			{
+				CWE:  "CWE-123",
+				CVSS: 5,
+			},
+			{
+				CWE:  "CWE-456",
+				CVSS: 7,
+			},
+		},
+		Info: report.SASTInfo{
+			SecurityScore: 5,
+			AverageCVSS:   7,
+		},
+	}
+
+	outResume := bytes.NewBufferString("")
+	r.Resume(outResume)
+	require.True(t, len(outResume.Bytes()) != 0)
+
+	outConsole := bytes.NewBufferString("")
+	r.Console(outConsole)
+	require.True(t, len(outConsole.Bytes()) != 0)
+}
+
+func TestReportHtml(t *testing.T) {
+	r := report.Report{
+		DRA: []report.DRA{{
+			Data:     "testing",
+			Type:     "email",
+			FilePath: "foo/bar",
+		}},
+		Vulnerabilities: []report.Vulnerability{
+			{
+				CWE:  "CWE-123",
+				CVSS: 5,
+			},
+		},
+		Info: report.SASTInfo{
+			SecurityScore: 5,
+		},
+		Total:  5,
+		High:   5,
+		Medium: 0,
+		Low:    0,
+	}
+
+	out := bytes.NewBufferString("")
+
+	err := r.Html(out)
+	require.Nil(t, err)
+	require.True(t, len(out.Bytes()) != 0)
+
+}
+
+func TestReportJson(t *testing.T) {
+	r := report.Report{
+		DRA: []report.DRA{{
+			Data:     "testing",
+			Type:     "email",
+			FilePath: "foo/bar",
+		}},
+	}
+
+	out := bytes.NewBufferString("")
+
+	err := r.Json(out)
+	require.Nil(t, err)
+	require.True(t, len(out.Bytes()) != 0)
+
+}
 
 func TestReportCleanDRA(t *testing.T) {
 	r := report.Report{
